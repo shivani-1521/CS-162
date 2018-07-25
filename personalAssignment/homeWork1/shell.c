f5321d3b4c329046b1eb8288eb5e794a9ea04e09
Shivani <f2016680@pilani.bits-pilani.ac.in>
2018-07-25 07:39:13 +0000

    added cd functionality to shell

--- personalAssignment/homeWork1/shell.c
+++ personalAssignment/homeWork1/shell.c
@@ -30,6 +30,7 @@ pid_t shell_pgid;
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
 int cmd_pwd(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -45,6 +46,7 @@ fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
   {cmd_pwd, "pwd", "print current working directory"},
+  {cmd_cd, "cd", "change to the directory entered"},
 };
 
 /* Prints a helpful description for the given command */
@@ -68,6 +70,15 @@ int cmd_pwd(unused struct tokens *tokens) {
   getcwd(buf,100);
   printf("\n %s \n", buf);
  
+}
+
+
+/*Change to the directory entered*/
+
+int cmd_cd(unused struct tokens *tokens) {
+  
+  chdir(tokens_get_token(tokens,1));
+  return 1;   
 }
 
 /* Looks up the built-in command, if it exists. */
